ecf455b0e66a5e2652887ff6576b90d66051eeb4
Filah Aditia <tech.filahaditia@gmail.com>
2023-03-08 23:05:15 +0700

    add build & push sh dan JenkinsFile

---
+++ docker-build-push.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# membuat sebuah Docker image menggunakan Dockerfile yang tadi dibuat dengan nama sampleapp-2 dan tag sesuai Build ID pada Jenkins
+docker build -t sampleapp-2:${BUILD_ID} .
+
+# melihat daftar image di lokal
+docker images
+
+# mengubah nama image agar sesuai dengan format Docker Hub
+docker tag sampleapp-2:${BUILD_ID} registry.hub.docker.com/filahaa/sampleapp-2:${BUILD_ID}
+
+# login ke Docker Hub
+docker login -u filahaa -p ${DOCKERHUB_PASSWORD}
+
+# mengunggah image ke Docker HUB
+docker push registry.hub.docker.com/filahaa/sampleapp-2:${BUILD_ID}
